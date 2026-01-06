d5daada16e079f87ed9765d81ae3071ad52ad4c5
flevett <freddie.levett@mobilitygiant.co.uk>
2026-01-06 16:12:32 +0000

    OOP

---
+++ src/lab7/Account.java
@@ -0,0 +1,50 @@
+package lab7;
+
+public class Account {
+    private int id;
+    private String owner;
+    private double balance;
+
+    // Constructor
+    public Account(int id, String owner, double balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    // Deposit method
+    void deposit(double amount) {
+        if (amount < 0) {
+            System.out.println("You cannot deposit a negative value");
+            return;
+        }
+        balance += amount;
+    }
+
+    // Withdraw method
+    void withdraw(double amount) {
+        if (amount < 0) {
+            System.out.println("You cannot withdraw a negative value");
+            return;
+        }
+
+        if (balance < amount) {
+            System.out.println("You cannot withdraw money you don't have...");
+            return;
+        }
+
+        balance -= amount;
+    }
+
+    // Add interest (2.5%)
+    public void addInterest() {
+        balance += balance * 0.025;
+    }
+
+    // Get account details
+    public String getDetails() {
+        return "Account ID: " + id +
+                ", Owner: " + owner +
+                ", Balance: £" + balance;
+    }
+}
---
+++ src/lab7/Program.java
@@ -0,0 +1,29 @@
+package lab7;
+
+public class Program {
+    public static void main(String[] args) {
+//        //create array of account references
+//        Account[] accounts = new Account[3];
+//
+//        //create account objects and store them
+//        accounts[0] = new Account(1, "Alice", 100);
+//        accounts[1] = new Account(2, "Bob", 200);
+//        accounts[2] = new Account(3, "Charlie", 300);
+//
+//        //display details
+//        for (Account acc:accounts) {
+//            System.out.println(acc.getDetails());
+//        }
+
+        Account myAccount = new Account(4, "Freddie", 100);
+        myAccount.addInterest();
+        System.out.println(myAccount.getDetails());
+
+        processAccount(myAccount);
+        System.out.println(myAccount.getDetails());
+    }
+
+    static void processAccount(Account acc){
+        acc.addInterest();
+    }
+}
